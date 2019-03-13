0b7743f2455b7bfea2921389f56b34d2f94cda10
Max Povinelli <povinellimax@gmail.com>
2019-03-13 18:04:40 -0400

    yay hexagon

--- src/Hexagon.js
+++ src/Hexagon.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import './Hexagon.css'
 
 class Hexagon extends Component{
-
   render() {
     return (
-      <div className="hexagon"><span></span></div>
+      <div className="hex">&#x2B22;</div>
     );
   }
 }
