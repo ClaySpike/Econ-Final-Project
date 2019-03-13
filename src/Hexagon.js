2332d8976e0bed4cec7c32dd5d8e279ad64eebe8
Max Povinelli <povinellimax@gmail.com>
2019-03-13 11:47:03 -0400

    Hexagon center screen

---
+++ src/Hexagon.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Hexagon.css'
+
+class Hexagon extends Component{
+
+  render() {
+    return (
+      <div className="hexagon"><span></span></div>
+    );
+  }
+}
+
+export default Hexagon;
