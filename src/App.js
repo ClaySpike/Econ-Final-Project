d0b165504aa3bbc166a9b787d6e7841f13ac2501
Max Povinelli <povinellimax@gmail.com>
2019-03-14 10:34:57 -0400

    hex columns and rows

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Hexagon from './Hexagon';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hexGrid: new Array(10),
+    };
+    for(let i = 0; i < this.state.hexGrid.length; i++){
+      this.state.hexGrid[i] = new Array(10);
+      for(let j = 0; j < this.state.hexGrid[i].length; j++){
+        this.state.hexGrid[i][j] = {
+          name: "Number " + (i + j),
+        };
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Hexagon></Hexagon>
-        </header>
+      <div className="HexGrid">
+        <div className="HexRow">{this.state.hexGrid.map((i) => {
+          return <div className={"HexCol Even"}>{this.state.hexGrid.map(() => {
+            return <Hexagon></Hexagon>
+          })}</div>
+        })}</div>
       </div>
     );
   }
