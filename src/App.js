0472f3f073f7b7be83946f0417f0654e55048519
Max Povinelli <povinellimax@gmail.com>
2019-03-15 11:41:33 -0400

    Hex thing yay it work

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import Hexagon from './Hexagon';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      hexGrid: new Array(10),
+      hexGrid: new Array(6),
     };
-    for(let i = 0; i < this.state.hexGrid.length; i++){//rows
-      this.state.hexGrid[i] = new Array(10);
-      for(let j = 0; j < this.state.hexGrid[i].length; j++){//cols
-        this.state.hexGrid[i][j] = {
-          name: "Number " + (i + j),
-        };
+    for(let i = 0; i < this.state.hexGrid.length; i++){
+      if(i % 2 === 0){
+        this.state.hexGrid[i] = new Array(10);
+      }
+      else{
+        this.state.hexGrid[i] = new Array(9);
+      }
+      for(let j = 0; j < this.state.hexGrid[i].length; j++){
+        this.state.hexGrid[i][j] = (<li className="hex">
+                                      <div className="hexIn">
+                                        <div className="hexLink">
+                                          <h1>This is a title</h1>
+                                          <p>Some sample text about the article this hexagon leads to</p>
+                                        </div>
+                                      </div>
+                                    </li>)
       }
     }
   }
 
+  giveHexes(){
+    let newArr = [];
+    for(var i = 0; i < this.state.hexGrid.length; i++){
+        newArr = newArr.concat(this.state.hexGrid[i]);
+    }
+    console.log(newArr);
+    return newArr;
+  }
+
   render() {
     return (
-      <div>
-        <div class="grid-container">
-        {this.state.hexGrid.map((value) => {//row
-          return <div>{value.map((i) => {//col
-            if(i == 0){
-              return <Hexagon class="evenRow"></Hexagon>
-            }
-            else{
-              return <Hexagon></Hexagon>
-            }
-          })}</div>
-        })}
-        </div>
-      </div>
+      <ul id="hexGrid">{ this.giveHexes() }
+        {/*<li class="hex">
+          <div class="hexIn">
+            <div class="hexLink">
+              <h1>This is a title</h1>
+              <p>Some sample text about the article this hexagon leads to</p>
+            </div>
+          </div>
+        </li>*/}
+      </ul>
     );
   }
 }
