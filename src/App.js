dfb85c5f541351b9b6b80c87ccfa764f830e93c9
Max Povinelli <povinellimax@gmail.com>
2019-03-18 14:32:37 -0400

    current iteration DONT DESTROY

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     this.state = {
       hexGrid: new Array(9),
+      nations: [{ ownedLocations: [] }],
     };
     /* START GRID INITIALIZATION */
     for(let i = 0; i < this.state.hexGrid.length; i++){
@@ -18,11 +19,13 @@ class App extends Component {
       }
       for(let j = 0; j < this.state.hexGrid[i].length; j++){
         this.state.hexGrid[i][j] = {
+          currentNation: -1,
           cropLevel: Math.random() * 10, 
           mineLevel: Math.random() * 10, 
           currentDiv: (<li className="hex" key={i + " " + j + " hex"}>
                         <div className="hexIn" key={i + " " + j + " hexIn"}>
                           <div className="hexLink" key={i + " " + j + " hexLink"}>
+                            <img></img>
                             <h1 key={i + " " + j + " data"}>error has occoured</h1>
                             <p key={i + " " + j + "text"}>Some sample text about the article this hexagon leads to</p>
                           </div>
@@ -35,44 +38,67 @@ class App extends Component {
     for(let i = 0; i < this.state.hexGrid.length; i++){
       for(let d = 0; d < this.state.hexGrid[i].length; d++){
         if(this.state.hexGrid[i][d].cropLevel > this.state.hexGrid[i][d].mineLevel){
-          this.state.hexGrid[i][d].currentDiv = (
-            <li className="hex" key={i + " " + d + " hex"}>
-              <div className="hexIn colorGreen" key={i + " " + d + " hexIn"}>
-                <div className="hexLink" key={i + " " + d + " hexLink"}>
-                  <h1 key={i + " " + d + " data"}>{this.state.hexGrid[i][d].cropLevel}</h1>
-                  <p key={i + " " + d + "text"}>Some sample text about the article this hexagon leads to</p>
-                </div>
-              </div>
-            </li>
-          )
+          this.setHexDiv(i, d, this.state.hexGrid[i][d].cropLevel, "Crop", "colorGreen")
         }
         else if(this.state.hexGrid[i][d].cropLevel < this.state.hexGrid[i][d].mineLevel){
-          this.state.hexGrid[i][d].currentDiv = (
-            <li className="hex" key={i + " " + d + " hex"}>
-              <div className="hexIn colorBrown" key={i + " " + d + " hexIn"}>
-                <div className="hexLink" key={i + " " + d + " hexLink"}>
-                  <h1 key={i + " " + d + " data"}>{this.state.hexGrid[i][d].mineLevel}</h1>
-                  <p key={i + " " + d + "text"}>Some sample text about the article this hexagon leads to</p>
-                </div>
-              </div>
-            </li>
-          )
+          this.setHexDiv(i, d, this.state.hexGrid[i][d].mineLevel, "Mine", "colorBrown")
         }
         else {
-          this.state.hexGrid[i][d].currentDiv = (
-            <li className="hex" key={i + " " + d + " hex"}>
-              <div className="hexIn" key={i + " " + d + " hexIn"}>
-                <div className="hexLink" key={i + " " + d + " hexLink"}>
-                  <h1 key={i + " " + d + " data"}>Error at this tile</h1>
-                  <p key={i + " " + d + "text"}>Some sample text about the article this hexagon leads to</p>
-                </div>
-              </div>
-            </li>
-          )
+          this.setHexDiv(i, d, "topText", "bottomText", "")
         }
       }
     }
     /* INITIALIZATION OF ARRAY WITH RAND VALUES COMPLETE*/
+    /* START INITIALIZATION OF NATIONS*/
+    for(let i = 0; i < 1; i++){
+      let xLoc = Math.floor((Math.random() * this.state.hexGrid.length))
+      let yLoc = Math.floor((Math.random() * this.state.hexGrid[xLoc].length))
+      this.state.nations[i] = { ownedLocations:[[xLoc, yLoc]] }
+      for(let j = 0; j < this.state.nations[i].ownedLocations.length; j++){
+        let xCoord = this.state.nations[i].ownedLocations[j][0];
+        let yCoord = this.state.nations[i].ownedLocations[j][1];
+        if(this.state.hexGrid[xCoord][yCoord].cropLevel > 
+          this.state.hexGrid[xCoord][yCoord].mineLevel){
+          this.setHexDiv(
+            xCoord, 
+            yCoord, 
+            this.state.hexGrid[xCoord][yCoord].cropLevel, 
+            "Crop", 
+            "colorGreenClaimed")
+        }
+        else if(this.state.hexGrid[xCoord][yCoord].cropLevel < 
+          this.state.hexGrid[xCoord][yCoord].mineLevel){
+          this.setHexDiv(
+            xCoord, 
+            yCoord, 
+            this.state.hexGrid[xCoord][yCoord].mineLevel, 
+            "Mine", 
+            "colorBrownClaimed")
+        }
+        else {
+          this.setHexDiv(
+            xCoord, 
+            yCoord, 
+            "topText", 
+            "bottomText", 
+            "")
+        }
+      }
+    }
+  }
+
+  setHexDiv(xLoc, yLoc, topText, bottomText, colorCSS){
+    this.state.hexGrid[xLoc][yLoc].currentDiv = (
+      <li className="hex" key={xLoc + " " + yLoc + " hex"}>
+        <div className={"hexIn " + colorCSS} key={xLoc + " " + yLoc + " hexIn"}>
+          <div className="hexLink" key={xLoc + " " + yLoc + " hexLink"}>
+            <img src={new Image(100, 100)}></img>
+            <h1 key={xLoc + " " + yLoc + " data"}>{topText}</h1>
+            <p key={xLoc + " " + yLoc + "text"}>{bottomText}</p>
+          </div>
+        </div>
+      </li>
+    )
   }
 
   giveHexes(){
