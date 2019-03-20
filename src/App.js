ffda13add2c798b9c2c021c58f01ae7dfac0f84f
Max Povinelli <povinellimax@gmail.com>
2019-03-20 14:40:55 -0400

    Working again now with SVG

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ class App extends Component {
     this.state = {
       width: 0, 
       height: 0,
-      hexGrid: new Array(15),
+      hexGrid: new Array(10),
     };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     for(let i = 0; i < this.state.hexGrid.length; i++){
       this.state.hexGrid[i] = new Array(this.state.hexGrid.length)
       for(let j = 0; j < this.state.hexGrid[i].length; j++){
-        let w = this.generateWater(2)
+        let w = this.generateWater(10)
+        let c = this.generateOther(w)
+        let m = this.generateOther(w)
         this.state.hexGrid[i][j] = {
           water: w,
-          cropLevel: this.generateOther(w),
-          mineLevel: this.generateOther(w),
-          div: this.setHexDiv(),
+          cropLevel: c,
+          mineLevel: m,
+          div: this.setHexDiv(w, c, m),
         }
-        console.log(this.state.hexGrid[i][j])
       }
     }
   }
@@ -40,7 +41,7 @@ class App extends Component {
   }
 
   generateWater(chance){
-    if(Math.floor(Math.random * chance) === 0){
+    if(Math.floor(Math.random() * chance) == 0){
       return true
     }
     return false
@@ -53,18 +54,34 @@ class App extends Component {
     return 0
   }
 
-  setHexDiv(){
-    return <svg height={this.getHexHeight(this.getHexWidth())} width={this.getHexWidth()}>
-            <polygon points= {this.getHexPoints()}
-            style={{ fill: "lime", stroke: "purple", strokeWidth: "5" }} />
-          </svg>
+  setHexDiv(water, cropLevel, mineLevel){
+    if(water){
+      return  <svg className="hex" width="9.5%" viewBox={"0 0 " + this.getHexWidth() + " " + this.getHexHeight()}>
+                <polygon points= {this.getHexPoints()} className="hexStyle hexColorBlue hexBackgroundStroke"/>
+              </svg>
+    }
+    else if(cropLevel > mineLevel){
+      return  <svg className="hex" width="9.5%" viewBox={"0 0 " + this.getHexWidth() + " " + this.getHexHeight()}>
+                <polygon points= {this.getHexPoints()} className="hexStyle hexColorGreen hexBackgroundStroke"/>
+              </svg>
+    }
+    else if(cropLevel < mineLevel){
+      return  <svg className="hex" width="9.5%" viewBox={"0 0 " + this.getHexWidth() + " " + this.getHexHeight()}>
+                <polygon points= {this.getHexPoints()} className="hexStyle hexColorBrown hexBackgroundStroke"/>
+              </svg>
+    }
+    else{
+      return  <svg className="hex" width="9.5%" viewBox={"0 0 " + this.getHexWidth() + " " + this.getHexHeight()}>
+                <polygon points= {this.getHexPoints()} className="hexStyle hexBackgroundStroke"/>
+              </svg>
+    }
   }
 
   getHexWidth(){
     if(document.getElementById("grid") != null){
-      return document.getElementById("grid").offsetWidth / 15
+      return document.getElementById("grid").clientWidth / 10
     }
-    return this.state.width / 15
+    return this.state.width / 10
   }
 
   getHexHeight(){
@@ -87,13 +104,16 @@ class App extends Component {
     let arr = []
     for(let i = 0; i < this.state.hexGrid.length; i++){
       for(let j = 0; j < this.state.hexGrid[i].length; j++){
-        this.state.hexGrid[i][j].div = this.setHexDiv()
+        this.state.hexGrid[i][j].div = this.setHexDiv(
+          this.state.hexGrid[i][j].water,
+          this.state.hexGrid[i][j].cropLevel,
+          this.state.hexGrid[i][j].mineLevel
+        )
       }
     }
     for(let i = 0; i < this.state.hexGrid.length; i++){
       arr = arr.concat(this.state.hexGrid[i])
     }
-    console.log(arr)
     return arr
   }
 
@@ -101,9 +121,9 @@ class App extends Component {
     return (
       <div className="App">
         <div id='grid'>
-          <div>{this.getHexes().map((value) => {
+          {this.getHexes().map((value) => {
             return value.div
-          })}</div>
+          })}
         </div>
       </div>
     );
