165219db76fd8087f07bab28c8723c9957eb7f46
Max Povinelli <povinellimax@gmail.com>
2019-03-20 11:26:25 -0400

    Hex horizontal grid needs spacing

--- src/App.js
+++ src/App.js
@@ -6,112 +6,24 @@ class App extends Component {
     super(props);
 
     this.state = {
-      hexGrid: new Array(9),
-      nations: [{ ownedLocations: [] }],
       width: 0, 
       height: 0,
+      hexGrid: new Array(15),
     };
     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    /* START GRID INITIALIZATION */
     for(let i = 0; i < this.state.hexGrid.length; i++){
-      if(i % 2 === 0){
-        this.state.hexGrid[i] = new Array(10)
-      }
-      else{
-        this.state.hexGrid[i] = new Array(9)
-      }
+      this.state.hexGrid[i] = new Array(this.state.hexGrid.length)
       for(let j = 0; j < this.state.hexGrid[i].length; j++){
+        let w = this.generateWater(2)
         this.state.hexGrid[i][j] = {
-          currentNation: -1,
-          cropLevel: Math.random() * 10, 
-          mineLevel: Math.random() * 10, 
-          currentDiv: (<li className="hex" key={i + " " + j + " hex"}>
-                        <div className="hexIn" key={i + " " + j + " hexIn"}>
-                          <div className="hexLink" key={i + " " + j + " hexLink"}>
-                            <img></img>
-                            <h1 key={i + " " + j + " data"}>error has occoured</h1>
-                            <p key={i + " " + j + "text"}>Some sample text about the article this hexagon leads to</p>
-                          </div>
-                        </div>
-                      </li>),
+          water: w,
+          cropLevel: this.generateOther(w),
+          mineLevel: this.generateOther(w),
+          div: this.setHexDiv(),
         }
+        console.log(this.state.hexGrid[i][j])
       }
     }
-
-    for(let i = 0; i < this.state.hexGrid.length; i++){
-      for(let d = 0; d < this.state.hexGrid[i].length; d++){
-        if(this.state.hexGrid[i][d].cropLevel > this.state.hexGrid[i][d].mineLevel){
-          this.setHexDiv(i, d, this.state.hexGrid[i][d].cropLevel, "Crop", "colorGreen")
-        }
-        else if(this.state.hexGrid[i][d].cropLevel < this.state.hexGrid[i][d].mineLevel){
-          this.setHexDiv(i, d, this.state.hexGrid[i][d].mineLevel, "Mine", "colorBrown")
-        }
-        else {
-          this.setHexDiv(i, d, "topText", "bottomText", "")
-        }
-      }
-    }
-    /* INITIALIZATION OF ARRAY WITH RAND VALUES COMPLETE*/
-    /* START INITIALIZATION OF NATIONS*/
-    for(let i = 0; i < 1; i++){
-      let xLoc = Math.floor((Math.random() * this.state.hexGrid.length))
-      let yLoc = Math.floor((Math.random() * this.state.hexGrid[xLoc].length))
-      this.state.nations[i] = { ownedLocations:[[xLoc, yLoc]] }
-      for(let j = 0; j < this.state.nations[i].ownedLocations.length; j++){
-        let xCoord = this.state.nations[i].ownedLocations[j][0];
-        let yCoord = this.state.nations[i].ownedLocations[j][1];
-        if(this.state.hexGrid[xCoord][yCoord].cropLevel > 
-          this.state.hexGrid[xCoord][yCoord].mineLevel){
-          this.setHexDiv(
-            xCoord, 
-            yCoord, 
-            this.state.hexGrid[xCoord][yCoord].cropLevel, 
-            "Crop", 
-            "colorGreenClaimed")
-        }
-        else if(this.state.hexGrid[xCoord][yCoord].cropLevel < 
-          this.state.hexGrid[xCoord][yCoord].mineLevel){
-          this.setHexDiv(
-            xCoord, 
-            yCoord, 
-            this.state.hexGrid[xCoord][yCoord].mineLevel, 
-            "Mine", 
-            "colorBrownClaimed")
-        }
-        else {
-          this.setHexDiv(
-            xCoord, 
-            yCoord, 
-            "topText", 
-            "bottomText", 
-            "")
-        }
-      }
-    }
-  }
-
-  setHexDiv(xLoc, yLoc, topText, bottomText, colorCSS){
-    this.state.hexGrid[xLoc][yLoc].currentDiv = (
-      <li className="hex" key={xLoc + " " + yLoc + " hex"}>
-        <div className={"hexIn " + colorCSS} key={xLoc + " " + yLoc + " hexIn"}>
-          <div className="hexLink" key={xLoc + " " + yLoc + " hexLink"}>
-            <img src={new Image(100, 100)}></img>
-            <h1 key={xLoc + " " + yLoc + " data"}>{topText}</h1>
-            <p key={xLoc + " " + yLoc + "text"}>{bottomText}</p>
-          </div>
-        </div>
-      </li>
-    )
-  }
-
-  giveHexes(){
-    let arr = []
-    for(let i = 0; i < this.state.hexGrid.length; i++){
-      for(let d = 0; d < this.state.hexGrid[i].length; d++){
-        arr.push(this.state.hexGrid[i][d].currentDiv)
-      }
-    }
-    return arr;
   }
 
   componentDidMount() {
@@ -127,12 +39,36 @@ class App extends Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
+  generateWater(chance){
+    if(Math.floor(Math.random * chance) === 0){
+      return true
+    }
+    return false
+  }
+
+  generateOther(water){
+    if(!water){
+      return Math.random() * 10
+    }
+    return 0
+  }
+
+  setHexDiv(){
+    return <svg height={this.getHexHeight(this.getHexWidth())} width={this.getHexWidth()}>
+            <polygon points= {this.getHexPoints()}
+            style={{ fill: "lime", stroke: "purple", strokeWidth: "5" }} />
+          </svg>
+  }
+
   getHexWidth(){
+    if(document.getElementById("grid") != null){
+      return document.getElementById("grid").offsetWidth / 15
+    }
     return this.state.width / 15
   }
 
-  getHexHeight(width){
-    return width * 2 * Math.tan(Math.PI / 6)
+  getHexHeight(){
+    return this.getHexWidth() * 2 * Math.tan(Math.PI / 6)
   }
 
   getHexPoints(){
@@ -142,27 +78,32 @@ class App extends Component {
     return halfWidth + ", " + 0 + " " +
     this.getHexWidth() + ", " + topHalf + " " + 
     this.getHexWidth() + ", " + bottomHalf + " " + 
-    halfWidth + ", " + this.getHexHeight(this.getHexWidth()) + " " +
+    halfWidth + ", " + this.getHexHeight() + " " +
     0 + ", " + bottomHalf + " " +
     0 + ", " + topHalf
   }
-  /*
-  (this.getHexWidth()/2) + ", " + 0 + " " +
-    this.getHexWidth() + ", " + (Math.tan(Math.PI/6)*this.getHexWidth()/2) + " " + 
-    this.getHexWidth() + ", " + (3*Math.tan(Math.PI/6)*this.getHexWidth()/2) + " " + 
-    (this.getHexWidth()/2) + ", " + this.getHexHeight() + " " +
-    0 + ", " + (3*Math.tan(Math.PI/6)*this.getHexWidth()/2) + " " +
-    0 + ", " + (Math.tan(Math.PI/6)*this.getHexWidth()/2)
-  */
+
+  getHexes(){
+    let arr = []
+    for(let i = 0; i < this.state.hexGrid.length; i++){
+      for(let j = 0; j < this.state.hexGrid[i].length; j++){
+        this.state.hexGrid[i][j].div = this.setHexDiv()
+      }
+    }
+    for(let i = 0; i < this.state.hexGrid.length; i++){
+      arr = arr.concat(this.state.hexGrid[i])
+    }
+    console.log(arr)
+    return arr
+  }
 
   render() {
     return (
       <div className="App">
-        <div id="grid">
-          <svg height={this.getHexHeight(this.getHexWidth())} width={this.getHexWidth()}>
-            <polygon points= {this.getHexPoints()}
-            style={{ fill: "lime", stroke: "purple", strokeWidth: "5" }} />
-          </svg>
+        <div id='grid'>
+          <div>{this.getHexes().map((value) => {
+            return value.div
+          })}</div>
         </div>
       </div>
     );
